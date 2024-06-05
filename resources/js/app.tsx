8b3850e86e123caf77eeff6112591c3bd4e083a5
yasunoh <checkupmaster@gmail.com>
2024-06-05 18:15:45 +0900

    フロント画面環境構築

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -6,6 +6,7 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+export const BASE_URL = import.meta.env.VITE_APP_URL;
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
